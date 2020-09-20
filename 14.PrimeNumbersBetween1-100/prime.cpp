b8a98e97bc58b191ff526d4f7b4652e6a5b2d830
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-20 23:38:26 +0545

    Files Added

--- 14.PrimeNumbersBetween1-100/prime.cpp
+++ 14.PrimeNumbersBetween1-100/prime.cpp
@@ -1,13 +1,23 @@
-//This is preprocessor command that tells the compiler to include the contents of stio.h//
+//This is preprocessor command that tells the compiler to include the contents of stio.h(Standard input and output) File in the program.
+
+//Stdio files contains functions such as (scanf() and printf () to take input and output )
+
 #include<stdio.h>
+
+//The execution of the program starts from the main finction.
 int main()
 {
-	int i,num, count;
-	printf("\n");
+	//Variable declare , Our output will be in number so we are taking int
 
+	int i,num, flag; 
+
+	//This is a library function to send formatted output to the screen. 
+	printf("The Prime Numbers are : \n");
+
+	// Main function
 	for(num = 1; num <= 100; num++)
 	{
-		count =  0;
+		flag =  0;
 		for (i = 2; i <= num/2; i++)
 		 
 		{
@@ -19,7 +29,7 @@ int main()
 		}
 
 		if(count == 0 && num !=1){
-			printf("%d \n",num);
+			printf("%d is prime number\n",num);
 		}
 	}
 	return 0;
