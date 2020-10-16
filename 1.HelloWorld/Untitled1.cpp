ea9edb1b6d6a809deedc9a8cff54ad11df3ca2a9
Aakrity Chapagai <aakritycpg@gmail.com>
2020-10-16 21:47:04 +0545

    First Program Files Added

--- 1.HelloWorld/Untitled1.cpp
+++ 1.HelloWorld/Untitled1.cpp
@@ -8,3 +8,19 @@ int main()
 }
 
 // Output : Hello,World!
+
+
+Note : The include is a preprocessor command which tells compiler to include the content of "stdio".
+
+2. stdio means: Standar input and Output file in the Program:
+The "stdio.h" file cotaint function (Scanf and printf to take input and display Output respecetively )
+
+3. If we only wtite printf but forget to write #include the Program will not compile
+
+4. The Execution of a c program start from the main() function.
+
+5. Prints is a libarary function which prints the content written in quote.
+
+6. return 0 is a statement, The program ends with this statement,
+
+
