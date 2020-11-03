a7101cd81a3d3899db893b570ee58e4ac288d908
Aakrity Chapagai <aakritycpg@gmail.com>
2020-11-03 23:24:42 +0545

    19th Program Files Added

--- 19.CheckAlphabetorNot/AlphaOrNot.c
+++ 19.CheckAlphabetorNot/AlphaOrNot.c
@@ -14,3 +14,7 @@ else
 	printf("%c is not an Alphabet.",c);
 return 0;
 }
+//In a C programming, a character variable holds an ASCII value can integer number
+// between 0 and 127 rather than that character.
+
+// The ASCII value of the lowercase alphabet is from 97 to 122 or from 65 to 90, that is num is an alphabet.
